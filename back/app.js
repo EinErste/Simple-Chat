f8c033375349b093b272462ac0ff1bdfb238b9a5
EinErste <37027381+EinErste@users.noreply.github.com>
2019-11-28 00:01:28 +0200

    Init&deploy

---
+++ back/app.js
@@ -0,0 +1,149 @@
+const express = require("express");
+const app = express();
+const messages = new Array();
+const users = [{username: "SYSTEM",password:"123"},{username: "Moderator",password:"123"},
+    {username: "Admin",password:"123"}];
+const power = [{username: "SYSTEM",password:"123"},{username: "Moderator",password:"123"},
+    {username: "Admin",password:"123"}];
+let counter = 0;
+app.set("view engine","ejs");
+app.use(express.static("front"));
+app.get("/",(req,res)=>{
+   res.render("../front/index");
+});
+server = app.listen(3000);
+const io = require("socket.io")(server);
+
+io.on('connection', (socket) => {
+    socket.emit("connection",messages);
+
+    socket.username = "Anonymous";
+
+    socket.on("disconnect", function() {
+        if(socket.username == "Anonymous") return;
+        sendSystemMessage(socket.username +" left chat");
+    });
+
+    socket.on("login", (user) => {
+        if(user.username == "Anonymous") {
+            socket.emit("alert","Reserved username");
+            return;
+        }
+        if(checkLogged(user)){
+            socket.emit("alert","User already logged");
+            return;
+        }
+        if(isRegistered(user)){
+            if(checkPassword(user)){
+                console.log(user);
+                if(socket.username!="Anonymous") {
+                    sendSystemMessage(socket.username +" left chat");
+                }
+                if(checkPower(user.username)) {
+                    socket.emit("power",true);
+                } else {
+                    socket.emit("power",false);
+                }
+                socket.username = user.username;
+            } else {
+                socket.emit("alert","Wrong login or password");
+                return;
+            }
+        } else {
+            if(!isValidUser(user)){
+                socket.emit("alert","Password or login cannot contain space or be empty string");
+                return;
+            }
+            if(socket.username!="Anonymous"){
+                sendSystemMessage(socket.username +" left chat");
+            }
+            socket.emit("power",false);
+            users.push(user);
+            socket.username = user.username;
+        }
+        sendSystemMessage(socket.username +" entered chat");
+    });
+
+    socket.on("new-message", (data) => {
+        if(messages.length>1000) messages.shift();
+        const messageObject = {message : data.message, username : socket.username, time : getTime(), id: counter++};
+        messages.push(messageObject);
+        io.sockets.emit("new-message", messageObject);
+    });
+
+    socket.on("delete-message-request",(data)=>{
+        if(!checkPower(socket.username)){
+            socket.emit("alert","Unauthorized access");
+            return;
+        }
+        removeMessageById(data);
+        io.sockets.emit("delete-message-confirmed", data);
+    })
+    function sendSystemMessage(string) {
+        const messageObject = {message : string, username : "SYSTEM", time : getTime(), id: counter++};
+        messages.push(messageObject);
+        io.sockets.emit("new-message", messageObject);
+    }
+
+});
+
+function getTime() {
+    var now = new Date();
+    var time = [now.getHours(), now.getMinutes(), now.getSeconds(),
+        now.getDate(),now.getMonth()+1,now.getFullYear()];
+
+    for(var i = 0; i < 3; i++) {
+        if(time[i] < 10) {
+            time[i] = '0' + time[i];
+        }
+    }
+
+    return time[0]+":"+time[1]+":"+time[2]+" "+ time[3]+"."+ time[4]+"."+ time[5];
+}
+
+function isRegistered(user) {
+    for (let i = 0; i < users.length; i++) {
+        if(user.username==users[i].username) return true;
+    }
+    return false;
+}
+
+function checkPassword(user) {
+    for (let i = 0; i < users.length; i++) {
+        if(user.password==users[i].password) return true;
+    }
+    return false;
+}
+
+function isValidUser(user) {
+    if(user.username.includes(" ")||user.password.includes(" ")) return false;
+    if(user.username==""||user.password=="") return false;
+    return true;
+}
+function checkLogged(user) {
+    const sockets = io.sockets.sockets;
+    for(var socketId in sockets) {
+        if(sockets[socketId].username==user.username){
+            return true;
+        }
+    }
+    return false;
+}
+
+function checkPower(username) {
+    for (let i = 0; i < power.length; i++) {
+        if(username==power[i].username) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function removeMessageById(id) {
+    for (let i = 0; i < messages.length; i++) {
+        if(id==messages[i].id) {
+            messages.splice(i,1);
+            break;
+        }
+    }
+}
---
+++ front/js/chat.js
@@ -0,0 +1,83 @@
+$(document).ready(function(){
+
+    $("#chatroom").scrollTop($("#chatroom")[0].scrollHeight);
+    var socket = io.connect('http://localhost:3000');
+    let power = false;
+    $("#message-send").click(function(){
+        socket.emit("new-message", {message : $("#message-input").val()})
+    });
+
+    socket.on("alert",(data)=>{
+        alert(data);
+    });
+
+    socket.on("new-message", (data) => {
+        $("#feedback").html('');
+        $("#message-input").val('');
+        renderMessage(data);
+        $("#chatroom").scrollTop($("#chatroom").get(0).scrollHeight);
+    });
+
+    socket.on("connection",(data)=>{
+        for (let i = 0; i < data.length; i++) {
+            renderMessage(data[i]);
+        }
+        $("#chatroom").scrollTop($("#chatroom").get(0).scrollHeight);
+    });
+
+    socket.on("power",data=> {
+        power = data;
+        if(power){
+            $(".message-delete").css("display","block");
+        } else {
+            $(".message-delete").css("display","none");
+        }
+    });
+
+    socket.on("delete-message-confirmed", (id)=>{
+        console.log(id);
+        $("#m"+id).remove();
+    });
+    function renderMessage(messageObj){
+        $("#chatroom").append("<div class=\"message\" id=\"m"+messageObj.id+"\">\n" +
+            "                <div class=\"message-info\">\n" +
+            "                    <div  class=\"author\">"+messageObj.username+"</div>\n" +
+            "                    <div class=\"time\">"+messageObj.time+"</div>\n" +
+            "                    <button class=\"message-delete\">x</button>\n" +
+            "                </div>\n" +
+            "                <div class=\"text\">"+messageObj.message+"</div>\n" +
+            "            </div>");
+        const user_left = messageObj.message.substring(messageObj.message.length-9,messageObj.message.length-5);
+        if(messageObj.username=="SYSTEM"){
+            if(user_left=="left"){
+                $("#m"+messageObj.id).css("background-color","lavender");
+            } else {
+                $("#m"+messageObj.id).css("background-color","darkseagreen");
+            }
+        }
+        if(power){
+            $(".message-delete").css("display","block");
+        } else {
+            $(".message-delete").css("display","none");
+        }
+    }
+
+    $("#login").click(function(){
+        socket.emit("login", {username : $("#username").val(),password: $("#password").val()})
+    });
+
+    $(document).keypress(function(e){
+        if (e.which == 13){
+            if($("#message-input").val()!=""){
+                $("#message-send").click();
+            }
+        }
+    });
+
+    $(document).on("click",".message-delete",function(e){
+        const messageDiv =  $(this).parent().parent();
+        const messageIdString = messageDiv.attr("id");
+        const messageId = messageIdString.substring(1,messageIdString.length);
+        socket.emit("delete-message-request",messageId);
+    });
+});
